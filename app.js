109d6ef05b2a8715087408fc7e4652927efaf976
Crisnzx <cristovao.neto@academico.ifpb.edu.br>
2021-07-08 12:30:10 -0300

    added division

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 Calculator = {
+
+   calculate: (num1, num2, operation) => {
+
+      if(operation === '/') {
+         return Calculator.div(num1, num2);
+      }
+
+   },
+
    sum: () => {
 
    },
@@ -11,7 +20,36 @@ Calculator = {
 
    },
 
-   div: () => {
-
+   div: (num1, num2) => {
+      return Math.round(num1 / num2);
    }
 }
+
+let operation = '+';
+
+document.querySelector('#operation').addEventListener('change', changeOperation);
+document.querySelector('form').addEventListener('submit', runApp);
+
+
+
+function changeOperation(e) {
+   operation = e.target.value;
+}
+
+function runApp(e) {
+   e.preventDefault();
+   let result;
+   let num1 = document.querySelector('#num1').value;
+   let num2 = document.querySelector('#num2').value;
+
+   if (operation === '/') {
+      result = Calculator.calculate(num1, num2, operation);
+   }
+
+   document.querySelector('#result').textContent = result;
+   
+   document.querySelector('#num1').value = '';
+   document.querySelector('#num2').value = '';
+}
+
+
